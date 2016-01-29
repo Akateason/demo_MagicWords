8eef9565b0db84687fc9e7f00286242972f5888b
akateason <xietianchen@subaojaojiang.com>
2016-01-29 18:13:11 +0800

    Initial Commit

---
+++ MagicWords/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MagicWords
+//
+//  Created by TuTu on 16/1/29.
+//  Copyright © 2016年 teason. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
